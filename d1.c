599ffb3507e8ffcae2bf73d5fafa2864b9c5f02b
Akshar Maitray <153086797+kannu74@users.noreply.github.com>
2024-09-23 15:27:16 +0530

    Update d1.c

--- d1.c
+++ d1.c
@@ -1,3 +1,74 @@
+Akhiyan meri poochh rahi hain
 
-chua choot maitray
-chraand aayush
+Dum ko mere chain nahi hai
+
+Kithe ladaiyan ve
+
+Tu akhiyan kithe ladaiyan ve
+
+Kaise tujhko main bataun
+
+Raahein teri takkti jaaun
+
+Neendan churaiyan ve
+
+Tu mewriyan neendan churaiyan ve
+
+Dhadkan yeh kehti hai
+
+Dil tere bin dhadke na
+
+Rs 1 Trial
+Ik tu hi yaar mera
+
+Mujhko kya duniya se lena
+
+Dhadkan yeh kehti hai
+
+Dil tere bin dhadke na
+
+Ik tu hi yaar mera
+
+Mujhko kya duniya se lena
+
+Tujhmein raat meri
+
+Tujhmein din mere
+
+Lamha ik jiyun na
+
+Main to bin tere
+
+Hai tere sath safar
+
+Jaana mujhe hai kidhar
+
+Ke beet jaaye tujhmein yeh umar
+
+Ik pal ki bhi ab to
+
+Doori na mujhko dena
+
+Ik tu hi yaar mera
+
+Mujhko kya duniya se lena
+
+Hissa hai hai tu ab to mere
+
+Dil ke jazbaton ka
+
+Tu lafz hai thehra hua
+
+Bas meri baaton ka
+
+Aankhein yeh kehti hain
+
+Tu saamne mere rehna
+
+Ik tu hi yaar mera
+
+Mujhko kya duniya se lena
+
+Ik tu hi yaar mera
+
+Mujhko kya duniya se lena
